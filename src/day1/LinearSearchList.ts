d5accda43c4b54cfdb1e7e8b38544d88dfc26340
Ruairí O'Brien <rtob@pm.me>
2023-10-17 18:45:03 +0100

    Added generated code

---
+++ src/day1/LinearSearchList.ts
@@ -0,0 +1,10 @@
+export default function linear_search(haystack: number[], needle: number): boolean {
+
+    for(const i in haystack) {
+        if(haystack[i] === needle) {
+            return true;
+        }
+    }
+    return false
+
+}
